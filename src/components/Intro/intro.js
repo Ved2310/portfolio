5aa5c6283ea87fc39edf19e3e3a8248a16468ba1
vedashree <vedb2000@gmail.com>
2025-04-13 15:49:33 -0700

    Updated Intro section styling and logic

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -6,11 +6,13 @@ import { Link } from 'react-scroll';
 
 const Intro = () => {
     return (
+
+
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm<span className="introName"> Vedashree</span><br />Software Developer</span>
-                <p className="introPara">I'm a Computer Science graduate student with 2.5+ years of experience in the fintech industry, now transitioning into data engineering. Passionate about building real-time, scalable data pipelines, I’ve worked with PySpark, AWS Glue, Redshift, and S3, and recently completed a certification in Apache Airflow DAG authoring. I'm currently pursuing Associate Cloud Engineer on Google Cloud. Excited to contribute to data-driven teams building modern, cloud-native data platforms!</p>
+                <p className="introPara">I'm a Computer Science graduate student with 2.5+ years of experience in the fintech industry, now transitioning into data engineering. I'm passionate about building real-time, scalable data pipelines, I’ve worked with PySpark, AWS Glue, Redshift, and S3, and recently completed a certification in Apache Airflow DAG authoring. I'm currently pursuing Associate Cloud Engineer on Google Cloud. I'm excited to contribute to data-driven teams building modern, cloud-native data platforms!</p>
                 <Link><button className="btn"><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
             </div>
             <div className="introImage">
